59c4fa1e8eadcfd2846796484783b587860d0c82
ygggggggggg <112917497+ygggggggggg@users.noreply.github.com>
2022-09-06 13:08:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,6 @@ var config = {
      */
     //imgs: {
     //    "心爱的小可爱": "./imgs/xiaokeai.png",
-        // "今天是你的生日": "./imgs/birthday.jpg",
     //},
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
